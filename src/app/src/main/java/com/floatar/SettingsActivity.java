40b1064b60b24739e85e85e8b2bd8fe9ddabcbfd
Óscar Alejandro Manteiga Seoane <manteigaseoaneoscar@gmail.com>
2023-04-12 13:59:12 +0200

    Settings activity

--- src/app/src/main/java/com/floatar/SettingsActivity.java
+++ src/app/src/main/java/com/floatar/SettingsActivity.java
@@ -1,19 +1,46 @@
 package com.floatar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+    private SwitchCompat notificationsSwitch;
+    private Spinner languageSpinner;
+    private SeekBar volumeSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
         setContentView(R.layout.activity_settings);
+
+        // Inicializar vistas
+        notificationsSwitch = findViewById(R.id.notificationsSwitch);
+        languageSpinner = findViewById(R.id.languageSpinner);
+        volumeSeekBar = findViewById(R.id.volumeSeekBar);
+
+        // Configurar opciones del spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.languages_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
+
+        // Configurar botón de guardar cambios
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Guardar cambios en SharedPreferences o en una base de datos
+                Toast.makeText(SettingsActivity.this, "Cambios guardados", Toast.LENGTH_SHORT).show();
+                finish(); // Regresar a la actividad anterior
+            }
+        });
     }
 }
