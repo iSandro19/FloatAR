3cc09e324ebfa39fa7bbf902c5223502bb6cfdd9
Óscar Alejandro Manteiga Seoane <oscar.manteiga@udc.es>
2023-04-15 14:32:02 +0200

    Update

--- src/app/src/main/java/com/floatar/CreateBoardActivity.java
+++ src/app/src/main/java/com/floatar/CreateBoardActivity.java
@@ -17,7 +17,6 @@ import android.widget.GridLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
--- src/app/src/main/java/com/floatar/SinglePlayerActivity.java
+++ src/app/src/main/java/com/floatar/SinglePlayerActivity.java
@@ -13,7 +13,6 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.Toast;
 
 public class SinglePlayerActivity extends AppCompatActivity {
 
