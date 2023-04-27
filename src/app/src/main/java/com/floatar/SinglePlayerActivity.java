d592e239346e768a22c7d8832d04e2969e0aa2df
Óscar Alejandro Manteiga Seoane <manteigaseoaneoscar@gmail.com>
2023-04-27 12:41:18 +0200

    Update single player layout

--- src/app/src/main/java/com/floatar/SinglePlayerActivity.java
+++ src/app/src/main/java/com/floatar/SinglePlayerActivity.java
@@ -14,6 +14,9 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class SinglePlayerActivity extends AppCompatActivity {
     private int[][] playerBoard = new int[10][10];
     private int[][] opponentBoard = new int[10][10];
@@ -55,7 +58,7 @@ public class SinglePlayerActivity extends AppCompatActivity {
             if (playerTurn) {
                 playerTurn(v, row, col);
             } else {
-                opponentTurn(v, row, col);
+                opponentTurn(v);
             }
         };
 
@@ -63,7 +66,7 @@ public class SinglePlayerActivity extends AppCompatActivity {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 Button button = new Button(this);
-                button.setTag("player_button_" + i + "_" + j); // Establecer una etiqueta única para cada botón
+                button.setTag("playerbutton_" + i + "_" + j); // Establecer una etiqueta única para cada botón
                 button.setBackgroundColor(ContextCompat.getColor(mContext, R.color.light_brown));
                 int buttonSize = (getScreenWidth()/2) / 10;
                 ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(buttonSize, buttonSize);
@@ -82,7 +85,7 @@ public class SinglePlayerActivity extends AppCompatActivity {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 Button button = new Button(this);
-                button.setTag("opponent_button_" + i + "_" + j); // Establecer una etiqueta única para cada botón
+                button.setTag("opponentbutton_" + i + "_" + j); // Establecer una etiqueta única para cada botón
                 button.setBackgroundColor(ContextCompat.getColor(mContext, R.color.light_brown));
                 int buttonSize = (getScreenWidth() - 100) / 10;
                 ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(buttonSize, buttonSize);
@@ -106,6 +109,10 @@ public class SinglePlayerActivity extends AppCompatActivity {
             // El jugador ha acertado
             opponentBoard[row][col] = 2; // Actualizar la matriz "myBoard"
             v.setBackgroundColor(ContextCompat.getColor(mContext, R.color.red)); // Cambiar el color del botón a rojo
+            if (checkGameOver(opponentBoard)) {
+                // El jugador ha ganado
+                System.out.println("El jugador ha ganado");
+            }
         } else {
             // El jugador ha fallado
             opponentBoard[row][col] = -1; // Actualizar la matriz "myBoard"
@@ -114,18 +121,68 @@ public class SinglePlayerActivity extends AppCompatActivity {
         }
     }
 
-    // Ejecutar la lógica del oponente
-    private void opponentTurn(View v, int row, int col) {
-        if (playerBoard[row][col] == 1) {
-            // El jugador ha acertado
-            playerBoard[row][col] = 2; // Actualizar la matriz "myBoard"
-            v.setBackgroundColor(ContextCompat.getColor(mContext, R.color.red)); // Cambiar el color del botón a rojo
-        } else {
-            // El jugador ha fallado
-            playerBoard[row][col] = -1; // Actualizar la matriz "myBoard"
-            v.setBackgroundColor(ContextCompat.getColor(mContext, R.color.gray)); // Cambiar el color del botón a gris
-            playerTurn = true; // Cambiar el turno al oponente
+    private void opponentTurn(View v) {
+        if (!playerTurn) {
+            int row, col;
+            Random random = new Random();
+
+            do {
+                row = random.nextInt(10);
+                col = random.nextInt(10);
+            } while (opponentBoard[row][col] != 0);
+
+            if (playerBoard[row][col] == 1) {
+                // El oponente ha acertado
+                opponentBoard[row][col] = 2; // Actualizar la matriz "opponentBoard"
+                //Button button = playerButtonGrid[row][col]; // Obtener el botón correspondiente
+                v.setBackgroundColor(ContextCompat.getColor(mContext, R.color.red)); // Cambiar el color del botón a rojo
+                if (checkGameOver(playerBoard)) {
+                    // El oponente ha ganado
+                    System.out.println("El oponente ha ganado");
+                } else {
+                    // El oponente escoge una posición adyacente de forma aleatoria
+                    int[] neighborRowOffsets = {-1, 0, 1, 0};
+                    int[] neighborColOffsets = {0, 1, 0, -1};
+                    ArrayList<Integer> validNeighbors = new ArrayList<>();
+                    for (int i = 0; i < neighborRowOffsets.length; i++) {
+                        int neighborRow = row + neighborRowOffsets[i];
+                        int neighborCol = col + neighborColOffsets[i];
+                        if (neighborRow >= 0 && neighborRow < 10 && neighborCol >= 0 && neighborCol < 10 && opponentBoard[neighborRow][neighborCol] == 0) {
+                            validNeighbors.add(i);
+                        }
+                    }
+                    if (validNeighbors.size() > 0) {
+                        int neighborIndex = validNeighbors.get(random.nextInt(validNeighbors.size()));
+                        int neighborRow = row + neighborRowOffsets[neighborIndex];
+                        int neighborCol = col + neighborColOffsets[neighborIndex];
+                        opponentBoard[neighborRow][neighborCol] = -1; // Marcar la posición como "fallida"
+                        //Button neighborButton = playerButtonGrid[neighborRow][neighborCol];
+                        v.setBackgroundColor(ContextCompat.getColor(mContext, R.color.gray)); // Cambiar el color del botón a gris
+                    }
+                }
+            } else {
+                // El oponente ha fallado
+                opponentBoard[row][col] = -1; // Actualizar la matriz "opponentBoard"
+                //Button button = playerButtonGrid[row][col]; // Obtener el botón correspondiente
+                v.setBackgroundColor(ContextCompat.getColor(mContext, R.color.black)); // Cambiar el color del botón a negro
+                playerTurn = true; // Cambiar el turno al jugador
+            }
         }
+    }
+
+    // Comprobar si el juego ha terminado
+    private boolean checkGameOver(int[][] board) {
+        boolean gameOver = true;
+        // Comprobar si quedan barcos en el tablero
+        for (int[] row : board) {
+            for (int cell : row) {
+                if (cell == 1) {
+                    gameOver = false;
+                    break;
+                }
+            }
+        }
+        return gameOver;
     }
 
     @Override
