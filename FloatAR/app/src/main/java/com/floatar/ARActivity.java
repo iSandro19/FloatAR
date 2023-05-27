a2b6dce44d72b662f232dce15a70d8164377e466
antonio.vila <antonio.vila@udc.es>
2023-05-27 18:02:33 +0200

    Inicio ARActivity

---
+++ FloatAR/app/src/main/java/com/floatar/ARActivity.java
@@ -0,0 +1,22 @@
+package com.floatar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.ar.core.Anchor;
+
+public class ARActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aractivity);
+
+    }
+}
